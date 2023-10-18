3e52c4b6e7ecce4e914698c8906c7966756531a4
Matthieu B <mesjeux0108@gmail.com>
2023-10-18 20:55:25 +0200

    test

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -8,5 +8,5 @@ const {
 } = process.env;
 require("dotenv").config();
 module.exports = {
-  url: DATABASE_URL
+  url: 'mongodb+srv://mesjeux0108:UGVW5NmsJJRpwXdV@cluster0.ph0tm0m.mongodb.net/poskin?ssl=true&replicaSet=atlas--shard-0&authSource=admin&retryWrites=true&w=majority'
 };
