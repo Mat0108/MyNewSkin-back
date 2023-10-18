1ea7198994474bd12b34024ac57463a22b2785a0
Coumbadiankha <coumba.diankha@hotmail.fr>
2023-10-18 14:18:29 +0200

    Update rdvModel.js

---
+++ server/app/models/rdvModel.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+let rdvSchema = new Schema({
+  IdentifiantClient: {
+    type: String,
+    required: true,
+  },
+  IdentifiantExpert: {
+    type: String,
+    required: true,
+  },
+  DateDebut: {
+    type: Date,
+    required: true,
+  },
+  DateFin: {
+    type: Date,
+    required: true,
+  },
+  Confirmation: {
+    type: Number,
+    required: true,
+  },
+  // On ajoute ici, les références aux utilisateurs (compte client et compte expert)
+  CompteClient: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+  },
+  CompteExpert: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+  },
+});
+
+module.exports = mongoose.model("Rdv", rdvSchema);
+
