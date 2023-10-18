de67d99ec7f339baa5f3646567ca610e9b6ef0b2
Matthieu B <mesjeux0108@gmail.com>
2023-10-18 15:23:02 +0200

    test server.js

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-
+const mongoose = require("mongoose")
 const app = express();
 
 // parse requests of content-type - application/json
@@ -16,11 +16,8 @@ const db = require("./app/models");
 
 console.log(db.url);
 
-db.mongoose
-  .connect(db.url, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  })
+mongoose
+  .connect("mongodb://admin:sVCIgSQGJTK76pv2eFdKEuiO@MongoS3601A.back4app.com:27017/2fb4ddc747cc4adbb0d8b9415add6eab ")
   .then(() => {
     console.log("Connected to the database!");
   })
@@ -41,48 +38,6 @@ userRoute(app);
 blogRoute(app);
 // const messageRoute = require("./app/routes/messageRoutes")
 const { application } = require('express');
-// Inscription d'utilisateur
-// app.post("/message/create", (req, res) => {
-//   let newMessage = new Message(req.body);
-//   newMessage.save((error, message) => {
-//       if (error) {
-//           res.status(401);
-//           console.log(error);
-//           res.json({ message: "Rêquete invalide" });
-//       }
-//       else {
-//           res.status(200);
-//           res.json(message);
-//       }
-//   })
-// })
-// app.get("/message/:messageId",(req,res)=>{
-//   Message.findById(req.params.messageId, (error, message) => {
-//       if (error) {
-//           res.status(401);
-//           console.log(error);
-//           res.json({ message: "Rêquete invalide" });
-//       }
-//       else {
-//           res.status(200);
-//           res.json(message);
-//       }
-//   })
-// })
-// app.get("/messages",(req,res)=>{
-//   Message.find({},(error,message)=>{
-//       if (error) {
-//           res.status(401);
-//           console.log(error);
-//           res.json({ message: "Rêquete invalide" });
-//       }
-//       else {
-//           res.status(200);
-//           res.json(message);
-//       }
-//   })
-// })
-// set port, listen for requests
 const PORT = process.env.NODE_DOCKER_PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
