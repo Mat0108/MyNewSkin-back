b29525b9c040852e317847337793e0365d3a567f
Coumbadiankha <coumba.diankha@hotmail.fr>
2023-11-08 09:46:41 +0100

    Update package.json et userController

--- server/app/controllers/userController.js
+++ server/app/controllers/userController.js
@@ -1,42 +1,64 @@
 const User = require("../models/userModel");
-
 const bcrypt = require("bcrypt");
+const nodemailer = require("nodemailer");
+
+// Configurez Nodemailer pour l'envoi d'e-mails
+const transporter = nodemailer.createTransport({
+  service: "Gmail",
+  auth: {
+    user: "votreadresse@gmail.com",
+    pass: "votremotdepasse",
+  },
+});
 
 // Inscription d'utilisateur
 exports.userRegister = (req, res, error) => {
-    let newUser = new User(req.body);
-    
-// Modification inscription user : si le mdp ou le mail ou le firstname et le lastname ne sont pas remplie alors pas d'inscription possible
-    if (newUser.password && newUser.email && newUser.firstname && newUser.lastname) {
-        bcrypt.hash(newUser.password, 10, (error, hash) => {
-            if (error) {
-                res.status(401);
-                console.log(error);
-                res.json({ message: "Impossible de crypter le mot de passe" });
-            }
-            else {
-                newUser.password = hash;
+  let newUser = new User(req.body);
 
-                newUser.save((error, user) => {
-                    if (error) {
-                        res.status(401);
-                        console.log(error);
-                        res.json({ message: "Rêquete invalide" });
-                    }
-                    else {
-                        res.status(200);
-                        res.json({ message: `Utilisateur crée : ${user.email}` });
-                    }
-                });
-            }
-        })
-    }
-    else {
+  // Modification inscription user : si le mdp ou le mail ou le firstname et le lastname ne sont pas remplis alors pas d'inscription possible
+  if (newUser.password && newUser.email && newUser.firstname && newUser.lastname) {
+    bcrypt.hash(newUser.password, 10, (error, hash) => {
+      if (error) {
         res.status(401);
-        res.json({ message: "Mot de passe est vide" });
         console.log(error);
-    }
-}
+        res.json({ message: "Impossible de crypter le mot de passe" });
+      } else {
+        newUser.password = hash;
+
+        newUser.save((error, user) => {
+          if (error) {
+            res.status(401);
+            console.log(error);
+            res.json({ message: "Requête invalide" });
+          } else {
+            // Envoi de l'e-mail de confirmation
+            const mailOptions = {
+              from: "votreadresse@gmail.com", // Adresse de l'expéditeur
+              to: user.email, // Adresse du destinataire
+              subject: "Confirmation d'inscription",
+              html: `Bienvenue sur notre site ! Cliquez sur le lien ci-dessous pour activer votre compte : <a href="https://votresite.com/activate/${user._id}">Activer le compte</a>`,
+            };
+
+            transporter.sendMail(mailOptions, (error, info) => {
+              if (error) {
+                console.log(error);
+              } else {
+                console.log("E-mail de confirmation envoyé : " + info.response);
+              }
+            });
+
+            res.status(200);
+            res.json({ message: `Utilisateur créé : ${user.email}` });
+          }
+        });
+      }
+    });
+  } else {
+    res.status(401);
+    res.json({ message: "Mot de passe est vide" });
+    console.log(error);
+  }
+};
 
 
 // Connexion d'utilisateur
@@ -182,23 +204,39 @@ exports.getUserById = (req, res) => {
 
 // Modifier tous les informations d'un utilisateur
 exports.updateUser = (req, res) => {
-    User.findOneAndUpdate({ userId: req.params.userId }, req.body, { new: true, runValidators: true })
-        .then(result => res.status(200).json({ message: "Utilisateur est bien mis à jour", result }))
-        .catch((error) => res.status(404).json({ message: "Utilisateur non trouvé" }))
-
+    User.findOneAndUpdate({ _id: req.params.userId }, req.body, { new: true, runValidators: true })
+      .then(user => {
+        if (!user) {
+          return res.status(404).json({ message: "Utilisateur non trouvé" });
+        }
+        res.status(200).json({ message: "Utilisateur est bien mis à jour", user });
+      })
+      .catch(error => res.status(500).json({ message: "Erreur serveur", error }));
 };
+  
 
-// Supprimer l'utilisateur
+/// Supprimer l'utilisateur
 exports.deleteUser = (req, res) => {
-    User.deleteOne({ _id: req.params.userId })
-        .then(result => res.status(200).json({ message: "Utilisateur est bien supprimé", result }))
-        .catch((error) => res.status(404).json({ message: "Utilisateur non trouvé" }))
+    User.findByIdAndRemove(req.params.userId)
+      .then(user => {
+        if (!user) {
+          return res.status(404).json({ message: "Utilisateur non trouvé" });
+        }
+        res.status(200).json({ message: "Utilisateur est bien supprimé", user });
+      })
+      .catch(error => res.status(500).json({ message: "Erreur serveur", error }));
 };
+  
 
 // Modifier quelques informations de l'utilisateur
 exports.patchUser = (req, res) => {
-    User.findByIdAndUpdate({ _id: req.params.userId }, req.body, { new: true })
-        .then(result => res.status(200).json({ message: "Utilisateur est bien mis à jour", result }))
-        .catch((error) => res.status(404).json({ message: "Utilisateur non trouvé" }))
-
+    User.findByIdAndUpdate(req.params.userId, req.body, { new: true })
+      .then(user => {
+        if (!user) {
+          return res.status(404).json({ message: "Utilisateur non trouvé" });
+        }
+        res.status(200).json({ message: "Utilisateur est bien mis à jour", user });
+      })
+      .catch(error => res.status(500).json({ message: "Erreur serveur", error }));
 };
+  
