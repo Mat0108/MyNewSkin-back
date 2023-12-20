cd4e431caa5b21df7fc881f2b57d8af3a2398147
Coumbadiankha <coumba.diankha@hotmail.fr>
2023-12-20 21:25:47 +0100

    JwtMiddleware.js

--- server/app/middlewares/jwtMiddleware.js
+++ server/app/middlewares/jwtMiddleware.js
@@ -1,5 +1,4 @@
 const jwt = require('jsonwebtoken');
-
 const jwtKey = process.env.JWT_KEY;
 
 exports.authenticate = (req, res, next) => {
@@ -21,3 +20,27 @@ exports.authenticate = (req, res, next) => {
         res.json({message: "Accès interdit : token manquant"});
     }
 }
+
+
+
+// const jwt = require('jsonwebtoken');
+// const crypto = require('crypto');
+// const jwtKey = process.env.JWT_KEY || crypto.randomBytes(32).toString('hex');
+
+// exports.authenticate = (req, res, next) => {
+//     let token = req.headers['authorization'];
+
+//     if (token !== undefined) {
+//         jwt.verify(token, jwtKey, (error, user) => {
+//             if (error) {
+//                 res.status(403);
+//                 res.json({ message: "Accès interdit : token invalide" });
+//             } else {
+//                 next();
+//             }
+//         });
+//     } else {
+//         res.status(403);
+//         res.json({ message: "Accès interdit : token manquant" });
+//     }
+// };
