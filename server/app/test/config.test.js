a07033d99e1ee707c9ba629e693739dbf9c8fc2c
Coumbadiankha <coumba.diankha@hotmail.fr>
2024-03-05 14:12:43 +0100

    Update config.test.js

--- server/app/test/config.test.js
+++ server/app/test/config.test.js
@@ -1,49 +1,36 @@
-// Importation des modules de test (Chai)
 const { expect } = require('chai');
 
-// Importation du module à tester
 const { ErrorMessage, logoBase64 } = require("../config/config");
 
-// Définir les tests pour ErrorMessage
 describe('ErrorMessage Function', () => {
     it('should return a JSON response with the provided message in production environment', () => {
-        // Simuler l'environnement de production
         process.env.ENV_TYPE = 'prod';
-
-        // Mock de la fonction de réponse
         const res = {
             json: (data) => {
                 expect(data).to.deep.equal({ message: 'Test Message' });
             }
         };
-
-        // Appel de la fonction ErrorMessage
         ErrorMessage(res, null, 'Test Message');
     });
 
     it('should return a JSON response with the provided error in non-production environment', () => {
-        // Simuler un environnement non production
         process.env.ENV_TYPE = 'dev';
 
-        // Mock de la fonction de réponse
         const res = {
             json: (error) => {
-                expect(error).to.be.an('object'); // Assurez-vous que l'erreur est un objet
-                expect(error.errorCode).to.equal(500); // Assurez-vous que le code d'erreur est 500
-                expect(error.errorMessage).to.equal('Internal Server Error'); // Assurez-vous que le message d'erreur est correct
+                expect(error).to.be.an('object'); 
+                expect(error.errorCode).to.equal(500); 
+                expect(error.errorMessage).to.equal('Internal Server Error');
             }
         };
 
-        // Appel de la fonction ErrorMessage
         ErrorMessage(res, { errorCode: 500, errorMessage: 'Internal Server Error' }, 'Test Message');
     });
 });
 
-// Définir les tests pour logoBase64
 describe('logoBase64 Function', () => {
     it('should return a base64-encoded PNG image string', () => {
         const result = logoBase64();
-        // Assurez-vous que le résultat est une chaîne valide commençant par "data:image/png;base64,"
         expect(result).to.be.a('string').and.to.match(/^data:image\/png;base64,/);
     });
 });
